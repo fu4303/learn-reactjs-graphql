d370dffc8172cc6af1904858f1aeeee7d9056423
Mohamed Samara <45723974+mohamedsamara@users.noreply.github.com>
2019-07-16 17:16:56 -0700

    add sass webpack configuration

--- client/app/index.js
+++ client/app/index.js
@@ -7,4 +7,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Import application sass styles
+import './styles/style.scss';
+
 ReactDOM.render(<div>React Works!</div>, document.getElementById('root'));
--- config/webpack/webpack.common.js
+++ config/webpack/webpack.common.js
@@ -1,7 +1,5 @@
 const path = require('path');
 const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const NODE_ENV = process.env.NODE_ENV;
@@ -24,52 +22,14 @@ module.exports = {
         test: /\.(js|jsx)$/,
         loader: 'babel-loader',
         exclude: /(node_modules)/
-      },
-      {
-        test: /\.(scss|sass|css)$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true,
-              modules: true,
-              importLoaders: 1,
-              camelCase: false,
-              localIdentName: '[local]'
-            }
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              plugins: () => [require('autoprefixer')],
-              sourceMap: true
-            }
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true
-            }
-          }
-        ]
       }
     ]
   },
-
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify(NODE_ENV)
       }
-    }),
-    new HtmlWebpackPlugin({
-      template: path.join(CURRENT_WORKING_DIR, 'client/public/index.html'),
-      inject: 'body'
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'css/[name].[hash].css',
-      disable: process.env.NODE_ENV !== 'production'
     }),
     new CopyWebpackPlugin([
       {
